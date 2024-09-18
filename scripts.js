986abccd681a30f6227b8849d37e73e25f4dc888
Geotryy <140184115+Geotryy@users.noreply.github.com>
2024-09-18 02:05:26 -0300

    tela de relatorio e atualizacao em layouts

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ const ctx = document.getElementById('financialChart').getContext('2d');
 new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['January', 'February', 'March', 'April'],
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril'],
         datasets: [{
-            label: 'Financial Overview',
+            label: 'Review Financeiro',
             data: [5000, 7000, 8000, 10000],
             backgroundColor: 'rgba(75, 192, 192, 0.6)',
             borderColor: 'rgba(75, 192, 192, 1)',
